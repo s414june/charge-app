c27cb177389bf9724a29a34475ee4ccb8a1821e4
雨芩 吳 <48972804+s414june@users.noreply.github.com>
2022-03-22 18:38:27 +0800

    update

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,63 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div id="helloContainer">
+        <h1>
+          hello world
+          <ion-icon :icon="heart"></ion-icon>
+          <ion-icon :icon="heart"></ion-icon>
+          <ion-icon :icon="heart"></ion-icon>
+          <ion-icon :icon="heart"></ion-icon>
+          <ion-icon :icon="heart"></ion-icon>
+          <ion-icon :icon="heart"></ion-icon>
+          <ion-icon :ios="logoApple" :md="logoAndroid"></ion-icon>
+        </h1>
+      </div>
+      <ExploreContainer name="Tab 1 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { heart,logoAndroid, logoApple } from "ionicons/icons";
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
+
+export default defineComponent({
+  name: "Tab1Page",
+  components: {
+    ExploreContainer,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  setup() {
+    return { heart,logoAndroid, logoApple };
+  },
+});
+</script>
+<style>
+#helloContainer {
+  display: flex;
+  justify-content: center;
+}
+</style>
